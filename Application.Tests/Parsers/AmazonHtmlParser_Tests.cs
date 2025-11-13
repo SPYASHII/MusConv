3921bf88504e52eefb2b65f0060e9e017221d0cf
Nikita <nikitaklime2005@gmail.com>
2025-11-13 20:55:00 +0200

    ADD: Test for AmazonHtmlParser

---
+++ Application.Tests/Parsers/AmazonHtmlParser_Tests.cs
@@ -0,0 +1,40 @@
+﻿using Application.Parsers;
+using Domain.Models;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Tests.Parsers
+{
+    [TestClass]
+    public class AmazonHtmlParser_Tests
+    {
+        [TestMethod]
+        public void GetPlaylist_WithAmazonHtmlDoc_ReturnsInfo()
+        {
+            var doc = new HtmlDocument();
+
+            doc.Load("Parsers/TestFiles/Playlist/amazon.html");
+
+            var parser = new AmazonHtmlParser();
+
+            var expected = new Playlist()
+            {
+                Name = "All Hits"
+            };
+
+            int numOfTracksExpected = 60;
+
+            //act
+            
+            var playlist = parser.GetPlaylist(doc);
+
+            //assert
+            Assert.AreEqual(expected.Name, playlist.Name);
+            Assert.AreEqual(numOfTracksExpected, playlist.Tracks.Count());
+        }
+    }
+}
